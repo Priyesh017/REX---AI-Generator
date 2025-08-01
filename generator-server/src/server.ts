0cc533b1c2b9b5d1014b6bee06d60e37dde27ef9
Priyesh Kumar Saha <priyeshkrs017@gmail.com>
2025-08-01 23:25:58 +0530

    Updated

--- generator-server/src/server.ts
+++ generator-server/src/server.ts
@@ -1,6 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
-import { supabase } from "./config/supabase";
 import { env } from "./config/env";
 import useRoute from "./routes/routes";
 
@@ -8,18 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Backend running ✅");
 });
-
-// app.get("/api/test-supabase", async (_, res) => {
-//   const { data, error } = await supabase.from("images").select("*").limit(1);
-//   if (error) {
-//     res.status(500).json({ error: error.message });
-//     return;
-//   }
-//   res.json(data);
-// });
 
 app.use("/api", useRoute);
 
